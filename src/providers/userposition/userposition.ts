9c4155d5d0dc2e38084f86ee4dd1dad8208fb933
Hope Tambala <hopetambala@outlook.com>
2018-10-14 23:28:07 -0400

    fix(cross_platform):

--- src/providers/userposition/userposition.ts
+++ src/providers/userposition/userposition.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 import { Geolocation, GeolocationOptions } from '@ionic-native/geolocation';
 
@@ -13,10 +14,11 @@ export class UserpositionProvider {
     longitude: null
   }
 
-  constructor(private geolocation:Geolocation) {
+  constructor(private geolocation:Geolocation,private plt:Platform) {
     console.log('Hello UserpositionProvider');
   }
 
+  /*
   public getUserPosition() {
     //Retrieves coordinates of the user
     this.options = {
@@ -26,6 +28,24 @@ export class UserpositionProvider {
     };
     
     return this.geolocation.getCurrentPosition(this.options)
+  }*/
+
+  async getUserPosition() {
+    this.options = {
+      enableHighAccuracy: true,
+      timeout: 30000,
+      //maximumAge: 0
+    };
+    
+    await this.plt.ready();
+    this.geolocation.getCurrentPosition(this.options).then((results)=>{
+      console.log(results.coords.latitude,results.coords.longitude)
+    })
+      
+    return await this.geolocation.getCurrentPosition(this.options)
+
+    
+
   }
  
 
