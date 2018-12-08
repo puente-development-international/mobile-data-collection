6d607a894c6e88cacb81f1a55060f1c39aa5ad6e
Hope Tambala <hopetambala@outlook.com>
2018-12-08 13:33:12 -0500

    fix(map records): limit number of records retrieved on page

--- src/providers/map-controls/map-controls.ts
+++ src/providers/map-controls/map-controls.ts
@@ -61,7 +61,7 @@ export class MapControlsProvider {
     /*
       Parse
     */
-    let limit = 500;
+    let limit = 100;
     //let parseClass = 'SurveyData';
     let parseClass = parseClassName;
     let parseField = 'surveyingOrganization';
